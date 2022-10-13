905e695b09acff52cea62ec627f8a2b44eb2126e
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-10-13 21:08:22 +0200

    Made routing table

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+//Front controller:
+
+//echo 'Requested URL = '.$_SERVER['QUERY_STRING'];
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+//echo get_class($router);
+
+$router->add('', ['controller'=>'Home', 'action'=>'index']);
+$router->add('posts', ['controller'=>'Posts', 'action'=>'index']);
+$router->add('posts/new', ['controller'=>'Posts', 'action'=>'new']);
+
+echo '<pre>';
+echo var_dump($router->getRoutes());
+echo '</pre>';
+
+?>
