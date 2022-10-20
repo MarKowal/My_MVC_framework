b407f43efb1e697e41a705841fe7822e7a7ba283
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-10-20 22:23:54 +0200

    Use regular expresion in controller and action

--- Core/Router.php
+++ Core/Router.php
@@ -2,14 +2,57 @@
 
 class Router{
     protected $routes = [];
+    protected $params = [];
 
+    /*
+    //dla stałego URL bez regex:
     public function add($route, $params){
         $this->routes[$route] = $params;
+    }*/
+
+    //teraz z uzyciem regex:
+    public function add($route, $params = []){
+        $route = preg_replace('/\//', '\\/', $route);
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+        $route = '/^'.$route.'$/i';
+        $this->routes[$route] = $params;
     }
+
     public function getRoutes(){
         return $this->routes;
     } 
 
+    //dla stałego URL bez regex:
+    /*public function match($url){
+        foreach($this->routes as $route=>$params){
+            if($url == $route){
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }*/
+
+    //teraz z uzyciem regex:
+    public function match($url){
+        foreach($this->routes as $route=>$params){
+            if(preg_match($route, $url, $matches)){
+                foreach($matches as $key=>$match){
+                    if(is_string($key)){
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }  
+        }
+        return false;
+    }
+
+
+    public function getParams(){
+        return $this->params;
+    }
 }
 
 
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 //Front controller:
 
-//echo 'Requested URL = '.$_SERVER['QUERY_STRING'];
+//echo 'Requested URL = '.$_SERVER['QUERY_STRING']."<br>";
 
 require '../Core/Router.php';
 
@@ -12,10 +12,25 @@ $router = new Router();
 
 $router->add('', ['controller'=>'Home', 'action'=>'index']);
 $router->add('posts', ['controller'=>'Posts', 'action'=>'index']);
-$router->add('posts/new', ['controller'=>'Posts', 'action'=>'new']);
+//$router->add('posts/new', ['controller'=>'Posts', 'action'=>'new']);
+//$router->add('blog', ['controller'=>'Blog', 'action'=>'index']);
+//$router->add('products/list', ['controller'=>'Products', 'action'=>'list']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+
 
 echo '<pre>';
 echo var_dump($router->getRoutes());
 echo '</pre>';
+
+//Match the requested route:
+$url = $_SERVER['QUERY_STRING'];
+if ($router->match($url)){
+    echo '<pre>';
+    echo var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo 'No route found for URL: '.$url;
+}
 
 ?>
