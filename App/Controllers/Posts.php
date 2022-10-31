ae9ce5d85ce0592c752cafaa3fb6e994fefd1d7b
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-10-31 16:52:08 +0100

    Render view and output escaping

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -6,10 +6,14 @@ class Posts extends \Core\Controller {
     public function indexAction(){
         echo "Hello from the index action in the Posts controller";
         //echo "<p>Query string parameters: <pre>".htmlspecialchars(print_r($_GET, true))."</pre></p>";
+        echo "<p>Route parameters: <pre>".htmlspecialchars(print_r($this->route_params, true))."</pre></p>";
+
     }
 
     public function addNewAction(){
         echo "Hello from the addNew action in the Postst controller";
+        echo "<p>Route parameters: <pre>".htmlspecialchars(print_r($this->route_params, true))."</pre></p>";
+
     }
 
     public function editAction(){
