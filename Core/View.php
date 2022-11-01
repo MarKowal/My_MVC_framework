b29fdaeac56f2bfa6bef389554e8dc7d4794ce13
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-11-01 20:35:51 +0100

    Before a base template to inherit

--- Core/View.php
+++ Core/View.php
@@ -12,6 +12,25 @@ class View{
             echo "$file not found";
         }
     }
+
+    public static function renderTemplate(string $template, array $args = []){
+        static $twig = null;
+
+        /*if($twig === null){
+            $loader = new \Twig_Loader_Filesystem(dirname(__DIR__) . '/App/Views'); 
+            $twig = new \Twig_Environment($loader);
+        }
+
+        echo $twig->render($template, $args);*/
+
+        if ($twig === null)
+        {
+            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
+            $twig = new \Twig\Environment($loader);
+        }
+ 
+        echo $twig->render($template, $args);
+    }
 }
 
 
