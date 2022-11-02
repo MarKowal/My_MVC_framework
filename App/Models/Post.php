32e65fdbb1e5ef9d2c6bce871eb4dba79196565b
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-11-02 21:38:00 +0100

    Optimise DB connection

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -4,20 +4,22 @@ namespace App\Models;
 
 use PDO;
 
-class Post{
+class Post extends \Core\Model{
     public static function getAll(){
-        $host = 'localhost';
+        /*$host = 'localhost';
         $dbname = 'mvc';
         $username = 'root';
-        $password = '';
+        $password = '';*/
 
         try{
-            $db = new PDO("mysql: 
+            /*$db = new PDO("mysql: 
             host = $host;
             dbname = $dbname;
             charset = utf8", 
             $username, 
-            $password);
+            $password);*/
+
+            $db = static::getDB();
 
             $stmt = $db->query('SELECT id, title, content FROM mvc.posts ORDER BY created_at');
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
