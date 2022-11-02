943a2d0e6a7d9d67da78a3ba17a482260df12f97
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-11-02 22:06:24 +0100

    Config file with DB connection credentials

--- Core/Model.php
+++ Core/Model.php
@@ -2,23 +2,27 @@
 namespace Core;
 
 use PDO;
+use App\Config;
 
 abstract class Model{
     protected static function getDB(){
         static $db = null;
         if($db === null){
-            $host = 'localhost';
+            /*$host = 'localhost';
             $dbname = 'mvc';
             $username = 'root';
-            $password = '';
+            $password = '';*/
 
             try{
-                $db = new PDO("mysql: 
+                /*$db = new PDO("mysql: 
                 host = $host;
                 dbname = $dbname;
                 charset = utf8", 
                 $username, 
-                $password);
+                $password);*/
+
+                $dsn = 'mysql:host='.Config::DB_HOST.';dbname='.Config::DB_NAME.';charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
 
             } catch(PDOexception $e) {
             echo $e->getMessage();
