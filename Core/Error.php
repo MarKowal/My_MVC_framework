dca473f40793eb4b76e9a1051b8e8285b8750651
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-11-11 21:02:30 +0100

    Error handling by HTTP codes 404 and 500

--- Core/Error.php
+++ Core/Error.php
@@ -10,6 +10,12 @@ class Error{
     }
 
     public static function exceptionHandler($exception){
+        $code = $exception->getCode();
+        if ($code != 404){
+            $code = 500;
+        }
+        http_response_code($code);
+
         if(\App\Config::SHOW_ERRORS){
             echo "<h1>Fatal error</h1>";
             echo "<p>Uncaught exception: '".get_class($exception)."'</p>";
@@ -25,7 +31,13 @@ class Error{
             $message .= "\nStack trace:".$exception->getTraceAsString();
             $message .= "\nThrown in '".$exception->getFile()."' on line ".$exception->getLine();
             error_log($message);
-            echo "<h1>An error occured!</h1>";
+           //echo "<h1>An error occured!</h1>";
+           /*if ($code == 404){
+                echo "<h1>Page not found!</h1>";
+           } else {
+                echo "<h1>An error occured!</h1>";
+           }*/
+           View::renderTemplate("$code.html");
 
         }
     }
--- Core/Router.php
+++ Core/Router.php
@@ -84,7 +84,7 @@ class Router{
             }
         } else {
             //echo "No route matched.";
-            throw new \Exception("No route matched");
+            throw new \Exception('No route matched.', 404);
         }
     }
 
